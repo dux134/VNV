b5e03aa3a2eb4ad3bee95a6eee6bf902962ac61d
Shubham <shubham.tomer06@gmail.com>
2019-04-20 05:03:14 +0530

    18 April 2019

---
+++ app/src/main/java/in/book/vnv/activitys/Test.java
@@ -0,0 +1,15 @@
+package in.book.vnv.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.book.vnv.R;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
