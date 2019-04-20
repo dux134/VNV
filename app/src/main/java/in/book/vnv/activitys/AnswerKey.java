4e191c9ae492d8e2214c7d1e9b2a951ee2d6e12b
Shubham <shubham.tomer06@gmail.com>
2019-04-20 15:03:41 +0530

    18 April 2019

---
+++ app/src/main/java/in/book/vnv/activitys/AnswerKey.java
@@ -0,0 +1,16 @@
+
+package in.book.vnv.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.book.vnv.R;
+
+public class AnswerKey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_key);
+    }
+}
